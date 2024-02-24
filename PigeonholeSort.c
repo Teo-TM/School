15c62fcac9967b066c19eb9e1d6bb484071790a6
Teo-TM <137792839+Teo-TM@users.noreply.github.com>
2024-02-25 00:12:24 +0100

    Update code

--- PigeonholeSort.c
+++ PigeonholeSort.c
@@ -9,33 +9,24 @@ int main() {
 }
 
 int* pigeonholeSort(int arr[], int size) {
-    int i, j;
+    int i, j = 0;
     int min = arr[0];
     int max = arr[0];
     int range;
 
-    for (i = 1; i < size; i++) {
-        if (arra_nums[i] < min_val) min_val = arra_nums[i];
-        if (arra_nums[i] > max_val) max_val = arra_nums[i];
+    for (i = 1; i < size; i ++) {
+        if (arr[i] < min_val) min = arr[i];
+        if (arr[i] > max_val) max = arr[i];
     }
 
-    range = max_val - min_val + 1;
-    int *temp = (int *)malloc(sizeof(int) * range);
+    range = max -min +1;
+    int *temp = (int *) calloc(sizeof(int) *range);
 
-    // Initialize the temp array with zeros
-    for (i = 0; i < range; i++) {
-        temp[i] = 0;
-    }
-
-    // Count the occurrences of each element in the input array
     for (i = 0; i < size; i++) {
-        temp[arra_nums[i] - min_val]++;
+        temp[arr[i] -min] ++;
     }
 
-    j = 0;
-
-    // Reconstruct the sorted array based on the counts
-    for (i = 0; i < range; i++) {
+    for (i = 0; i < range; i ++) {
         while (temp[i] > 0) {
             arra_nums[j] = i + min_val;
             temp[i]--;
