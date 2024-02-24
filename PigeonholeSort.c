904646fd6d992cd30f79e5bdac633ae4178c82c1
Teo-TM <137792839+Teo-TM@users.noreply.github.com>
2024-02-25 00:08:08 +0100

    Update code

--- PigeonholeSort.c
+++ PigeonholeSort.c
@@ -1,33 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+int main() {
     const int N = 10;
     int arr[N];
 
     return 0;
 }
 
-void sort(int arr[], int size){
+int* pigeonholeSort(int arr[], int size) {
+    int i, j;
     int min = arr[0];
     int max = arr[0];
+    int range;
 
-    for (int i = 1; i < size; i++){
-        if (arr[i] < min) min = arr[i];
-        if (arr[i] > max) max = arr[i];
+    for (i = 1; i < size; i++) {
+        if (arra_nums[i] < min_val) min_val = arra_nums[i];
+        if (arra_nums[i] > max_val) max_val = arra_nums[i];
     }
 
-    int range = max -min +1;
-    int holes[size][range];
-    int j = 0;
- 
-    for (int i = 0; i < size; i++){
-        holes[arr[i] -min][j] = arr[i];
-        j++;
-    }
-    int k = 0;
+    range = max_val - min_val + 1;
+    int *temp = (int *)malloc(sizeof(int) * range);
 
-    for (int i = 0; i < range; i++){
-       for (it = holes[i].begin(); it != holes[i].end(); ++it)
-            arr[index++]  = *it;
+    // Initialize the temp array with zeros
+    for (i = 0; i < range; i++) {
+        temp[i] = 0;
     }
+
+    // Count the occurrences of each element in the input array
+    for (i = 0; i < size; i++) {
+        temp[arra_nums[i] - min_val]++;
+    }
+
+    j = 0;
+
+    // Reconstruct the sorted array based on the counts
+    for (i = 0; i < range; i++) {
+        while (temp[i] > 0) {
+            arra_nums[j] = i + min_val;
+            temp[i]--;
+            j++;
+        }
+    }
+
+    return arra_nums;
 }
