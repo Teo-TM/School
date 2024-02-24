845b876462b572094c7905e5a84fd72917ed55a4
Teo-TM <137792839+Teo-TM@users.noreply.github.com>
2024-02-25 00:04:02 +0100

    Update code

--- PigeonholeSort.c
+++ PigeonholeSort.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello world");
+    const int N = 10;
+    int arr[N];
+
     return 0;
+}
+
+void sort(int arr[], int size){
+    int min = arr[0];
+    int max = arr[0];
+
+    for (int i = 1; i < size; i++){
+        if (arr[i] < min) min = arr[i];
+        if (arr[i] > max) max = arr[i];
+    }
+
+    int range = max -min +1;
+    int holes[size][range];
+    int j = 0;
+ 
+    for (int i = 0; i < size; i++){
+        holes[arr[i] -min][j] = arr[i];
+        j++;
+    }
+    int k = 0;
+
+    for (int i = 0; i < range; i++){
+       for (it = holes[i].begin(); it != holes[i].end(); ++it)
+            arr[index++]  = *it;
+    }
 }
